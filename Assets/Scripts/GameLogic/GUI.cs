11b5192b9b57900e616707f1e1fa423d3e8097a0
404 <404>
2016-04-09 23:29:57 +0200

    Redone GameInit

--- Assets/Scripts/GameLogic/GUI.cs
+++ Assets/Scripts/GameLogic/GUI.cs
@@ -1,27 +1,87 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GUI : MonoBehaviour
 {
+    //PlayerCreation
+    public GameObject SpaceShip;
+    public List<Vector3> PlayerPossition = new List<Vector3>(4);
+    private int PlayerCount = 0;
+    private List<Player> Players = new List<Player>();
+    private Button myButton;
+
+    //GUI
+    public GameObject PlayerGUI;
+    public List<Vector3> PlayerGuiPosition = new List<Vector3>(4);
+    private Text TitleText;
+    private Text StartText;
+    private Text QuitText;
     private GameObject gui;
-    //Initiate New Game
-    void InitGame()
+
+    void Start()
     {
-        gui = GetComponent<GameObject>();
+        StartText = GameObject.Find("StartText").GetComponent<Text>();
+        QuitText = GameObject.Find("QuitText").GetComponent<Text>();
+        TitleText = GameObject.Find("TitleText").GetComponent<Text>();
+        gui = GameObject.Find("StartGUI").GetComponent<GameObject>();
+
+        myButton = GameObject.Find("Start").GetComponent<Button>();
+        myButton.onClick.AddListener(Init);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        while (PlayerCount < 4 && PlayerCount > 0)
+        {
+            CreatePlayer();
+        }
+    }
+
+    // Use this for initialization
+    void Init()
+    {
+        gui.SetActive(false);
+        CreatePlayer();
+    }
+
+    //Creating Players
+    void CreatePlayer()
+    {
+        Players.Add(new Player()
+        {
+            PlayerID = PlayerCount,
+            Ship = Instantiate(SpaceShip, PlayerPossition[PlayerCount], Quaternion.identity) as GameObject,
+            GUI = Instantiate(PlayerGUI, PlayerPossition[PlayerCount], Quaternion.identity) as GameObject,
+        });
+        PlayerCount++;
     }
 
     //GameOver or Win
     public void GameOver()
     {
-        var text = GetComponentInChildren<Text>();
+        for (var i = 0; i <= PlayerCount; i++)
+        {
+            Destroy(Players[i].GUI);
+            Destroy(Players[i].Ship);
+        }
+        PlayerCount = 0;
+        TitleText.text = "Game Over";
         gui.SetActive(true);
     }
 
-    //Air Tank
-    void AirTank()
+    //Return Current Players
+    public List<Player> getPlayers()
     {
-        //TODO
+        return Players;
+    }
+
+    //Return Number of Current Players
+    public int getPlayerCount()
+    {
+        return PlayerCount;
     }
 
     //Comets Drop
@@ -35,10 +95,11 @@ public class GUI : MonoBehaviour
     {
         //TODO
     }
+}
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
+public class Player
+{
+    public int PlayerID { get; set; }
+    public GameObject Ship { get; set; }
+    public GameObject GUI { get; set; }
 }
