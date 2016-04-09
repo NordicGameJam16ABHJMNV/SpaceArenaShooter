888711b5832a6dbc493a0e15512710549f00e84a
BNicolas <bertram@fam-nicolas.dk>
2016-04-09 13:41:30 +0200

    added spaceship prefab, GameLogic object and InputController object

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputController : MonoBehaviour
+{
+    //Use this to handle inputs and call into the other scripts on the spaceships
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public float maxSpeed = 10f;
+    public float timeToMaxSpeed = 1f;
+
+    private float velocity = 0f; //TODO: MAKE CALCULATION!
+    private float friction;
+
+
+    // MaxSpeed = velocity/friction
+    // Time to max speed = 2.0/friction
+    // velocity = acceleration / fiction
+
+    //velocity =+ acceleration * deltatime
+    //velocity =- velocity * friction * deltatime
+
+
+    // Use this for initialization
+    void Start()
+    {
+        friction = 2.0f / timeToMaxSpeed;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int acceleration;
+
+    }
+}
