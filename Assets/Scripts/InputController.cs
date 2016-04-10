ea5f0151075b821b09177e44241ea18ddc4e2c99
BNicolas <bertram@fam-nicolas.dk>
2016-04-10 02:23:03 +0200

    Added WASD+Spacebar as 4th controller

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -25,18 +25,57 @@ public class InputController : MonoBehaviour
             {
                 PlayerController script = playerShip.GetComponent<PlayerController>();
 
-                Vector2 input = new Vector2(Input.GetAxis("HorizontalP" + (i + 1)), Input.GetAxis("VerticalP" + (i + 1)));
-
-                script.Move(input);
-                if (Input.GetKey("joystick " + (i + 1) + " button 0"))
+                if (i == 3)
                 {
-                    script.Shoot();
+                    float horizontal = 0.0f;
+                    float vertical = 0.0f;
+                    if (Input.GetKey("w"))
+                    {
+                        horizontal = 1.0f;
+                    }
+                    else
+                    {
+                        if (Input.GetKey("s"))
+                        {
+                            horizontal = -1.0f;
+                        }
+                    }
+
+                    if (Input.GetKey("a"))
+                    {
+                        vertical = -1.0f;
+                    }
+                    else
+                    {
+                        if (Input.GetKey("d"))
+                        {
+                            vertical = 1.0f;
+                        }
+                    }
+
+                    Vector2 input = new Vector2(vertical, horizontal);
+                    script.Move(input);
+                    if (Input.GetKey("space"))
+                    {
+                        script.Shoot();
+                    }
+
+                }
+                else
+                {
+                    Vector2 input = new Vector2(Input.GetAxis("HorizontalP" + (i + 1)), Input.GetAxis("VerticalP" + (i + 1)));
+
+                    script.Move(input);
+                    if (Input.GetKey("joystick " + (i + 1) + " button 0"))
+                    {
+                        script.Shoot();
+                    }
                 }
             }
         }
 
         //Pressing the start button
-        if(Input.GetKeyDown("joystick button 7"))
+        if (Input.GetKeyDown("joystick button 7"))
         {
             GameObject.Find("Game").GetComponent<GameLogic>().GameOver();
         }
